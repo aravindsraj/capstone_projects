cf43451509b8806029cc2c704f8d644c7a60cee1
Aravind <66876078+aravindsraj@users.noreply.github.com>
2021-06-22 17:16:35 +0530

    Update readme.py

--- week5/readme.py
+++ week5/readme.py
@@ -1,1 +1,7 @@
+### This files are uploded on 22/06/2021.
 
+1. Full Report : The battle of neighborhood report.pdf
+  
+2. Presentation : The Battle of neighborhoods ppt .pdf
+  
+3. Code : The Battle of Neighborhoods KL.ipynb
